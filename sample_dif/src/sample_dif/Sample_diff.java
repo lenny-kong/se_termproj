03caeba2c76e74977bb19f12e9f18929431e42e2
jisuAn <ajs7270@naver.com>
2018-05-01 15:29:43 +0900

    Complete String LCS algorithm.
    파일 입출력만 하면 알고리즘은 완성입니다.

--- sample_dif/src/sample_dif/Sample_diff.java
+++ sample_dif/src/sample_dif/Sample_diff.java
@@ -5,15 +5,25 @@ import java.util.*;
 public class Sample_diff {
 
 	public static void main(String[] args) {
-		List<String> sample = new ArrayList<String>();
-		// TODO Auto-generated method stub
-		Diff dif = new Diff();
-		String a = "The lovely little tiny valuable man in the universe.";
-		String b = "The tiny little valuable girl in the universe.";
-		sample = dif.syntax_Analysis_diff(a, b);
-		System.out.println(sample);
-		System.out.println("Different word : " + sample);
-		sample = dif.syntax_Analysis_identical(b, a);
-		System.out.println("Identical word" + sample);
+		//List<String> sample = new ArrayList<String>();
+		
+		String[] text_left = new String[] 
+				{
+					"0","1","2","3","50","4","5","6","9"
+				};
+		String[] text_right = new String[] 
+				{
+					"3","30","4","5","6","7","8","9","10"
+				};
+		
+		List<String_object> sample_l = new ArrayList<String_object>();
+		List<String_object> sample_r = new ArrayList<String_object>();
+
+		for(int i = 0; i < 9; i++) {
+			sample_l.add(new String_object(text_left[i]));
+			sample_r.add(new String_object(text_right[i]));
+		}
+		Diff_string differ = new Diff_string(sample_l, sample_r);
+		differ.print_List();
 	}
 }
