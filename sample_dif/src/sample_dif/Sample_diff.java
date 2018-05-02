4dbc853d8782d5d35f81cfca1fa72f2942be06d5
wonseok-kang <38484908+wonseok-kang@users.noreply.github.com>
2018-05-02 20:46:11 +0900

    Revert "Revert "Merge branch 'master' of https://github.com/lenny-kong/se_termproj""
    This reverts commit bfe94d393cc68d2331f6b72149eda211e36f7a25.

--- sample_dif/src/sample_dif/Sample_diff.java
+++ sample_dif/src/sample_dif/Sample_diff.java
@@ -1,50 +1,10 @@
 package sample_dif;
 
-import java.util.*;
-
 public class Sample_diff {
 
 	public static void main(String[] args) {
+		Diff_string differ1 = new Diff_string("compare_1.txt","compare_2.txt");
+		differ1.print_List();
 		
-		String[] text_left = new String[] 
-				{
-						"int main(String args[])",
-						"{",
-						"int i = 0;",
-						"int j = 0;",
-						"i = i + j;",
-						"printf(i + j);",
-						"return 0;",
-						"}"
-				};
-		String[] text_right = new String[] 
-				{
-						"package tweeter;",
-						"class instagram",
-						"{",
-						"int main(String args[])",
-						"{",
-						"int i = 0;",
-						"int k = 0;",
-						"int j = 0;",
-						"k = i + j;",
-						"printf(i + j);",
-						"return 0;",
-						"}",
-						"}"
-				};
-		
-		List<String_object> sample_l = new ArrayList<String_object>();
-		List<String_object> sample_r = new ArrayList<String_object>();
-
-		for(int i = 0; i < text_left.length; i++) {
-			sample_l.add(new String_object(text_left[i]));
-			
-		}
-		for(int i = 0; i < text_right.length; i++) {
-			sample_r.add(new String_object(text_right[i]));
-		}
-		Diff_string differ = new Diff_string(sample_l, sample_r);
-		differ.print_List();
 	}
 }
