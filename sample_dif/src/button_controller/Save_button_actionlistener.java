36395d586cb1fb11d2c8a5697f0164980dd7bec6
hataeyunn <gkxodbsa@gmail.com>
2018-05-28 15:43:55 +0900

    save // file chooser를 켰다가 아무런 파일을 선택안하고 취소를 눌렀을 경우 예외처리
    file 값에 null이 들어가기 때문에 다음 savefile 함수에서 문제가 생겼었음.
    이를 if(file!=null)을 추가해서 예외처리함.

--- sample_dif/src/button_controller/Save_button_actionlistener.java
+++ sample_dif/src/button_controller/Save_button_actionlistener.java
@@ -18,20 +18,21 @@ public class Save_button_actionlistener implements ActionListener {
 		// TODO Auto-generated method stub
 		int result = 0; // return value (confirm dialog)
 		File file;
-		
-		Object[] options = {"save left","save right","cancel"};
+
+		Object[] options = { "save left", "save right", "cancel" };
 		Save_button save_button = (Save_button) e.getSource();
 		this.gui_data_model = save_button.getGui_data_model();
 
-
-		//text to list;
+		// text to list;
 		result = JOptionPane.showOptionDialog(null, "which one do u wanna save?", "save",
-				JOptionPane.YES_NO_CANCEL_OPTION,JOptionPane.QUESTION_MESSAGE,null,options,options[0]);
+				JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
 		if (result == JOptionPane.YES_OPTION) {
 			file = save_button.getSaveFile(gui_data_model.getLeft_name());
 			try {
 				save_button.text_to_list(1);
-				save_button.savefile(file,gui_data_model.getLeft_list());
+				if (file != null) {
+					save_button.savefile(file, gui_data_model.getLeft_list());
+				}
 			} catch (IOException e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
@@ -41,7 +42,9 @@ public class Save_button_actionlistener implements ActionListener {
 			file = save_button.getSaveFile(gui_data_model.getRight_name());
 			try {
 				save_button.text_to_list(0);
-				save_button.savefile(file,gui_data_model.getRight_list());
+				if (file != null) {
+					save_button.savefile(file, gui_data_model.getRight_list());
+				}
 			} catch (IOException e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
