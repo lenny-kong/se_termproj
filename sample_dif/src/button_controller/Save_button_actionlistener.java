726b83b6ebfe0b685368e6361eaa2367ad5d0cc0
SimDaebeom <debum93@gmail.com>
2018-05-29 01:47:17 +0900

    save_button/actionlisener 추가
    원래 apply_button_to_gui 브랜치에 구현되어있던거 안들어가 있어서 추가합니다.

---
+++ sample_dif/src/button_controller/Save_button_actionlistener.java
@@ -0,0 +1,58 @@
+package button_controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
+import button_model.Save_button;
+import data_model.GUI_data_model;
+
+public class Save_button_actionlistener implements ActionListener {
+	private GUI_data_model gui_data_model;
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		int result = 0; // return value (confirm dialog)
+		File file;
+
+		Object[] options = { "save left", "save right", "cancel" };
+		Save_button save_button = (Save_button) e.getSource();
+		this.gui_data_model = save_button.getGui_data_model();
+
+		// text to list;
+		result = JOptionPane.showOptionDialog(null, "which one do u wanna save?", "save",
+				JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
+		if (result == JOptionPane.YES_OPTION) {
+			file = save_button.getSaveFile(gui_data_model.getLeft_name());
+			try {
+				save_button.text_to_list(1);
+				if (file != null) {
+					gui_data_model.setLeft_name(file.getName());
+					gui_data_model.getLeft_path_area().setText("File name : " + gui_data_model.getLeft_name());
+					save_button.savefile(file, gui_data_model.getLeft_list());
+				}
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+		if (result == JOptionPane.NO_OPTION) {
+			file = save_button.getSaveFile(gui_data_model.getRight_name());
+			try {
+				save_button.text_to_list(0);
+				if (file != null) {
+					gui_data_model.setRight_name(file.getName());
+					gui_data_model.getRight_path_area().setText("File name : " + gui_data_model.getRight_name());
+					save_button.savefile(file, gui_data_model.getRight_list());
+				}
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+	}
+}
--- sample_dif/src/button_model/Save_button.java
+++ sample_dif/src/button_model/Save_button.java
@@ -1,7 +1,22 @@
 package button_model;
 
 import java.awt.Dimension;
+import java.awt.Frame;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import button_controller.Load_button_actionlistener;
+import button_controller.Save_button_actionlistener;
+import compare_algorithm.String_object;
 import data_model.GUI_data_model;
 import window_view.Icon_image_load;
 
@@ -10,8 +25,10 @@ public class Save_button extends Button_model {
 	private final String default_image_path = "icon_image/save/save.png";
 	private final String pushed_image_path = "icon_image/save/save_push.png";
 	private final String mouseover_image_path = "icon_image/save/save_mouseover.png";
+	private GUI_data_model gui_data_model;
 
 	public Save_button(GUI_data_model gui_data_model) {
+
 		// construct and set default image.
 		super(Icon_image_load.load_image("icon_image/save/save.png"));
 
@@ -32,5 +49,94 @@ public class Save_button extends Button_model {
 		this.setBorderPainted(false);
 		// set button size.
 		this.setPreferredSize(new Dimension(24, 24));
+
+		Save_button_actionlistener actionlistener = new Save_button_actionlistener();
+		this.addActionListener(actionlistener);
+		this.setGui_data_model(gui_data_model);
+	}
+
+	public File getSaveFile(String fileName) {
+		// create frame
+		Frame f = new Frame();
+		File fileTemp = null;
+		String fileNameTemp;
+		// create file dialog
+		JFileChooser fileChooser = new JFileChooser();
+		fileChooser.setCurrentDirectory(new File(System.getProperty("user.dir")));
+		// create file filter
+		fileChooser.setAcceptAllFileFilterUsed(false);
+		FileNameExtensionFilter filter = new FileNameExtensionFilter("TXT (*.txt)", "txt");
+		fileChooser.addChoosableFileFilter(filter);
+
+		// recursive, input previous file name
+		if (fileName != "") {
+			fileChooser.setSelectedFile(new File(fileName));
+		}
+
+		// print file dialog 
+		if (fileChooser.showSaveDialog(f) == JFileChooser.APPROVE_OPTION) {
+
+			// if user doesn't add file extension
+			fileNameTemp = fileChooser.getSelectedFile().toString();
+			if (!fileNameTemp.endsWith(".txt") && !fileNameTemp.endsWith(".txt")) {
+				fileNameTemp += ".txt";
+			}
+
+			// if file that has same name is exist
+			fileTemp = new File(fileNameTemp);
+			if (fileTemp.exists()) {
+				int r = JOptionPane.showConfirmDialog(f, fileTemp.getName() + "이(가) 이미 있습니다. 바꾸시겠습니까?", "다른 이름으로 저장 확인",
+						JOptionPane.YES_NO_OPTION);
+				if (r == JOptionPane.NO_OPTION) {
+					// recursive
+					getSaveFile(fileNameTemp);
+				}
+			}
+
+		}
+
+		return fileTemp;
+	}
+
+	public void savefile(File file, List<String_object> list) throws IOException {
+		int cnt = 0;
+		BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+		for (cnt = 0; cnt != list.size(); cnt++) {
+			bw.write(list.get(cnt).get_string());
+			bw.newLine();
+		}
+		bw.close();
+	}
+	public void text_to_list(int value){		
+		if(value == 1) {
+			StringTokenizer left_token = new StringTokenizer(gui_data_model.getLeft_text_area().getText(), "\n");
+			// tokenize for insert that  data in list
+			int cnt;
+			gui_data_model.getLeft_list().clear();
+			// delete before data in left or right list
+			
+			for (cnt = left_token.countTokens(); cnt > 0; cnt--) {
+				gui_data_model.getLeft_list().add(new String_object(left_token.nextToken()));
+			}
+		}
+		if(value == 0) {
+			StringTokenizer right_token = new StringTokenizer(gui_data_model.getRight_text_area().getText(), "\n");
+			// tokenize for insert that  data in list
+			int cnt;
+			gui_data_model.getRight_list().clear();
+			// delete before data in left or right list
+			
+			for (cnt = right_token.countTokens(); cnt > 0; cnt--) {
+				gui_data_model.getLeft_list().add(new String_object(right_token.nextToken()));
+			}
+		}
+	}
+
+	public GUI_data_model getGui_data_model() {
+		return gui_data_model;
+	}
+
+	public void setGui_data_model(GUI_data_model gui_data_model) {
+		this.gui_data_model = gui_data_model;
 	}
 }
